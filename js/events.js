7f8286a9f8ebe0d1257640162672ec04690046eb
Al3nMicL <https://twitter.com/Al3nMicL>
2019-04-10 20:41:31 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,44 @@
 //define functions here
 
+// Task 1
+function getIt (){
+	$('p').on('click', function(){
+		alert("Hey!");
+	});
+}
+
+// Task 2
+function frameIt (){
+	$('img').on('load', function(){
+		$(this).addClass('tasty');
+	});
+}
+
+// Task 3
+function pressIt (){
+	$('#typing').on('keydown', function(event){
+		if (event.which == 71) {
+			alert('you pressed the g key!');
+		}
+	});	
+}
+
+// Task 4
+function submitIt (){
+	$('form').on('submit', function(){
+		alert('Your form is going to be submitted now.');
+	})	
+}
+
 $(document).ready(function(){
 
 // call functions here
+getIt(); // Task 1
+
+frameIt(); // Task 2
+
+pressIt(); // Task 3
+
+submitIt(); // Task 4
 
 });
